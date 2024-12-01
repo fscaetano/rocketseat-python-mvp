844b29e011118f00e12b8d4686cea40e1010526e
Fabricio da Silva Caetano <fscaetano@gmail.com>
2024-12-01 16:34:08 -0300

    Adding dependency call checks to unit tests.

--- src/models/sqlite/repositories/pets_repository_test.py
+++ src/models/sqlite/repositories/pets_repository_test.py
@@ -30,8 +30,21 @@ def test_list_pets():
     repo = PetsRepository(mock_connection)
     response = repo.list_pets()
 
-    print()
-    print(response)
+    mock_connection.session.query.assert_called_once_with(PetsTable)
+    mock_connection.session.all.assert_called_once()
+    mock_connection.session.filter.assert_not_called()
 
     name_matches = response[0].name == "rex"
     assert name_matches
+
+
+def test_delete_pet():
+    mock_connection = MockConnection()
+    repo = PetsRepository(mock_connection)
+
+    repo.delete_pet("petName")
+
+    mock_connection.session.query.assert_called_once_with(PetsTable)
+    mock_connection.session.filter.assert_called_once_with(
+        PetsTable.name == "petName")
+    mock_connection.session.delete.assert_called_once()
