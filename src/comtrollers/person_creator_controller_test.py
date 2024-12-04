f5f4bc393196348ce9b953b9a203740adba0a1ea
Fabricio da Silva Caetano <fscaetano@gmail.com>
2024-12-04 00:00:05 -0300

    feat: creating person_creator_controller

---
+++ src/comtrollers/person_creator_controller_test.py
@@ -0,0 +1,36 @@
+from pytest import raises
+from .person_creator_controller import PersonCreatorController
+
+
+class MockPeopleRepository:
+    def insert_person(self, first_name: str, last_name: str, age: int, pet_id: int):
+        pass
+
+
+def test_create():
+    person_info = {
+        "first_name": "Fulano",
+        "last_name": "deTal",
+        "age": 30,
+        "pet_id": 123
+    }
+
+    controller = PersonCreatorController(MockPeopleRepository())
+    response = controller.create(person_info)
+
+    assert response["data"]["type"] == "Person"
+    assert response["data"]["count"] == 1
+    assert response["data"]["attributes"] == person_info
+
+
+def test_create_error():
+    person_info = {
+        "first_name": "Fulano",
+        "last_name": "de Tal",
+        "age": 30,
+        "pet_id": 123
+    }
+
+    controller = PersonCreatorController(MockPeopleRepository())
+    with raises(Exception):
+        controller.create(person_info)
