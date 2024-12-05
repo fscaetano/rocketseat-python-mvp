b174b2323006a2cc2f6f41955821db3fc28ac0ef
Fabricio da Silva Caetano <fscaetano@gmail.com>
2024-12-05 00:34:05 -0300

    feat: Implementing validators with pydantic

---
+++ src/validators/person_creator_validator_test.py
@@ -0,0 +1,17 @@
+from src.validators.person_creator_validator import person_creator_validator
+
+
+class MockRequest:
+    def __init__(self, body) -> None:
+        self.body = body
+
+
+def test_person_creator_validator():
+    request = MockRequest({
+        "first_name": "Fulano",
+        "last_name": "deTal",
+        "age": 30,
+        "pet_id": 123
+    })
+
+    person_creator_validator(request)
--- src/views/person_creator_view.py
+++ src/views/person_creator_view.py
@@ -1,5 +1,6 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.validators.person_creator_validator import person_creator_validator
 from src.controllers.interfaces.person_creator_controller import PersonCreatorControllerInterface
 from .interfaces.view_interface import ViewInterface
 
@@ -9,6 +10,7 @@ class PersonCreatorView(ViewInterface):
         self.__controller = controller
 
     def handle(self, http_request: HttpRequest) -> HttpResponse:
+        person_creator_validator(http_request)
         person_info = http_request.body
         body_response = self.__controller.create(person_info)
         return HttpResponse(status_code=201, body=body_response)
